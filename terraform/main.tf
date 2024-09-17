c4341f417e4a5d030d743f53d1a4fc733552363e
starvos <khchung87@gmail.com>
2024-09-17 21:09:39 +0800

    assignment 3.11

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ module "ecs" {
       memory = 1024
       # Container definition(s)
       container_definitions = {
-        khchung-sample = { #container name
+        ecs-sample = { #container name
           essential = true
           image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${local.prefix}-ecr:latest"
           port_mappings = [
